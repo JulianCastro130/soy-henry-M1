7a0f3378ca2cb2f953e36cf9de4c541694ebbb03
Julian <cococastro619@gmail.com>
2022-11-07 16:39:13 -0300

    Homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let resultado = 0
+  const numero = num.split("").reverse().join("")
+  for (let i = 0; i < numero.length; i++) {
+    resultado += Math.pow(2, i) * numero[i]    
+  }
+  return resultado
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let resultado = []
+  do {
+    resultado.unshift(num % 2)
+    num = Math.trunc(num / 2)
+  } while (num >= 1)
+  return resultado.join("")
 }
 
 
